fb20b0fb63de1aabead6cc12fce8e931aca146e0
AMiller113 <ajayj46@gmail.com>
2019-05-28 16:56:24 -0400

    StockTrader Initial Commit

---
+++ StockTrader/StockTrader/StockTrader/Classes/StockContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StockTrader.Classes
+{
+    class StockContext : DbContext
+    {
+        public StockContext() : base("Server = (LocalDB)\\MSSQLLocalDB;Database=StockTraderDB;Integrated Security = True;") {}
+        public DbSet<StockEntity> stocks { get; set; }
+    }
+}
