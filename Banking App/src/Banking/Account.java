0e6d9a41f2eef41ef867cb12326b41402876d2e3
AMiller113 <ajayj46@gmail.com>
2019-08-16 13:06:53 -0400

    Revert "Medical Log"
    This reverts commit 4d071c4745d9d2621c9090ef8254e46a60d39b54.

--- Banking App/src/Banking/Account.java
+++ Banking App/src/Banking/Account.java
@@ -8,24 +8,23 @@ import java.util.List;
 
 public class Account implements java.io.Serializable {
 
+	/**
+	 * 
+	 */
 	private static final long serialVersionUID = 1L;
-	private final List<String> account_holders;
 	private float account_balance;
 	private final Date account_opened_date;
 	
-	public Account(List<String> account_holders,float initialBalance,Date accountOpenedDate){
-		this.account_holders = account_holders;
+	public Account(float initialBalance,Date accountOpenedDate){
 		this.account_balance = initialBalance;
 		this.account_opened_date = accountOpenedDate;
 	}	
 	
 	public float getAccount_balance(){ return account_balance; }
 	
+	public void setAccountBalnce(float newBalance){ this.account_balance = newBalance; }
+	
 	public Date getAccount_opened_date() { return account_opened_date; }
-	
-	public List<String> getAccount_holders() { return account_holders; }
-	
-	public void setAccountBalnce(float newBalance) { this.account_balance = newBalance; }
 	
 	public synchronized boolean DepositFunds(float depositAmount){
 		if(depositAmount <= 0){
@@ -70,12 +69,7 @@ public class Account implements java.io.Serializable {
 	
 	private synchronized void SaveAccountState(){
 		try {
-			StringBuilder sb = new StringBuilder();
-			for(String x : this.account_holders){
-				sb.append(x);
-			}
-			String path = sb.toString().concat(".ser");
-			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(path));
+			ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream(""));
 			out.writeObject(this);
 			out.close();
 		} catch (FileNotFoundException e) {
@@ -84,4 +78,34 @@ public class Account implements java.io.Serializable {
 			e.printStackTrace();
 		}
 	}
+}
+
+class SingleAccount extends Account{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private final String account_holder;
+	
+	public String getAccount_holder(){ return account_holder; }
+	
+	public SingleAccount(String accountHolder,float initialBalance,Date accountOpenedDate){
+		super(initialBalance,accountOpenedDate);
+		this.account_holder = accountHolder;
+	}	
+}
+
+class JointAccount extends Account{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private final List<String> account_holders;
+	
+	public JointAccount(List<String> accountHolders,float initialBalance,Date accountOpenedDate){
+		super(initialBalance,accountOpenedDate);
+		this.account_holders = accountHolders;
+	}
+	
+	public List<String> getAccount_holders() { return account_holders; }
 }
--- Banking App/src/Banking/Customer.java
+++ Banking App/src/Banking/Customer.java
@@ -3,30 +3,34 @@ package Banking;
 import java.util.List;
 import java.util.Scanner;
 
-public class Customer implements java.io.Serializable {
-	
-	private static final long serialVersionUID = 1L;
+public class Customer {
 	private String user_name;
 	private int password;
 	private List<Account> accounts;
 
-	public List<Account> getAccounts() { return accounts; }
+	public List<Account> getAccounts() {
+		return accounts;
+	}
 
-	public String getUser_name() { return user_name; }
-	
-	public int getPassword() { return password; }
-	
-	public void setAccounts(List<Account> accounts) { this.accounts = accounts; }
+	public void setAccounts(List<Account> accounts) {
+		this.accounts = accounts;
+	}
 
-	public void setUser_name(String user_name) { this.user_name = user_name;}
+	public String getUser_name() {
+		return user_name;
+	}
 
-	public void setPassword(){ 
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Please enter your new password");
-		String password =  scanner.nextLine();
-		int passwordHash = password.hashCode();
-		this.password = passwordHash;
-		}
+	public void setUser_name(String user_name) {
+		this.user_name = user_name;
+	}
+
+	public int getPassword() {
+		return password;
+	}
+
+	public void setPassword(int password) {
+		this.password = password;
+	}
 	
 	public void DepositFunds(){
 		Scanner scanner = new Scanner(System.in);
@@ -39,13 +43,5 @@ public class Customer implements java.io.Serializable {
 	public void TransferFunds(){
 		Scanner scanner = new Scanner(System.in);
 	}
-	
 	public void ApplyForAccount(){}
-	
-	public boolean ValidatePassword(String potentialPassword){
-		if(potentialPassword.matches()){
-			
-		}
-		return true;
-	}
 }
